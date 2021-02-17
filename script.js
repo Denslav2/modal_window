b281dbb23846afdec2d55d7acb42093af3d30fbd
Den_Slav <joomlakop@gmail.com>
2021-02-17 21:29:37 +0200

    modal

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const modalOpen = document.querySelector('[data-open]'),
+    modal = document.querySelector('.modal'),
+    modalClose = document.querySelector('[data-close]');
+
+    modalOpen.addEventListener('click', () =>{
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+    });
+
+    modalClose.addEventListener('click', () => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+    });
