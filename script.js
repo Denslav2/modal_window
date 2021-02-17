e89a6d9532b95619404b168cc49abbc2ab5fd41f
Den_Slav <joomlakop@gmail.com>
2021-02-17 21:33:27 +0200

    fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const modalOpen = document.querySelector('[data-open]'),
     modal = document.querySelector('.modal'),
     modalClose = document.querySelector('[data-close]');
 
-    modalOpen.addEventListener('click', () =>{
+    modalOpen.addEventListener('click', () => {
         modal.classList.add('show');
         modal.classList.remove('hide');
     });
